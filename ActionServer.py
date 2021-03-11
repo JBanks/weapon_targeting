c60e40606b086c120e7b8799944f5523e64bc069
Jeremy Banks <JBanks@users.noreply.github.com>
2021-03-11 15:23:28 -0400

    PEP cleanup
    Various PEP8 formatting changes

--- ActionServer.py
+++ ActionServer.py
@@ -14,26 +14,31 @@ pythonState = False
 env = Sim.Simulation(Sim.mergeState)
 
 
-class AStarAgent():
+class AStarAgent:
 	def getAction(self, state):
 		solution, __ = JS.AStar(state)
 		return solution[0]
 
-agent = AStarAgent() #Sim.JeremyAgent()
+
+agent = AStarAgent()  # Sim.JeremyAgent()
+
+
 def CompareResults(pythonState, unityState):
 	print(f"U-Effector energy remaining: {unityState[:, 0, JF.EffectorFeatures.ENERGYLEFT]}")
 	print(f"U-Effector time remaining: {unityState[:, 0, JF.EffectorFeatures.TIMELEFT]}")
 	print(f"P-Effector energy remaining: {pythonState[:, 0, JF.EffectorFeatures.ENERGYLEFT]}")
 	print(f"P-Effector time remaining: {pythonState[:, 0, JF.EffectorFeatures.TIMELEFT]}")
 
+
 @app.route('/', methods=['GET'])
 def test_connection():
 	return jsonify('The connection to the server was successful')
 
+
 @app.route('/', methods=['POST'])
 def get_action():
 	global pythonState
-	#print(f"received: {request.data}")
+	# print(f"received: {request.data}")
 	json_string = urllib.parse.unquote(request.data.decode("UTF-8"))
 	problem = json.loads(json_string)
 	for key in problem.keys():
@@ -44,7 +49,8 @@ def get_action():
 		CompareResults(pythonState, state)
 	action = agent.getAction(problem)
 	print(f"Selected action: {action}")
-	pythonState, _, _ = env.update_state((action[0], action[1]), state)
+	pythonState, _, _ = env.update_state((action[0], action[1]), state.copy())
 	return jsonify({'assets': [int(action[0]), int(action[1])]})
+
 
 app.run()
--- TrainSetGeneration.py
+++ TrainSetGeneration.py
@@ -13,14 +13,17 @@ import sys
 import re
 import os
 
+
 def log(string):
     print(f"[{time.asctime()}] {string}")
+
 
 def uuid_url64():
     """Returns a unique, 16 byte, URL safe ID by combining UUID and Base64
     """
     rv = base64.b64encode(uuid.uuid4().bytes).decode('utf-8')
     return re.sub(r'[\=\+\/]', lambda m: {'+': '-', '/': '_', '=': ''}[m.group(0)], rv)
+
 
 if __name__ == '__main__':
     effectors = 4
