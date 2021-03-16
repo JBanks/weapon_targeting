4044ad52e1a6ace2c3556078e98dedefcbd865f9
AlexSalgo <alex.salgo@live.ca>
2021-03-15 22:43:02 -0400

    Set up JFA Solver.

--- TrainSetGeneration.py
+++ TrainSetGeneration.py
@@ -24,6 +24,64 @@ def uuid_url64():
     rv = base64.b64encode(uuid.uuid4().bytes).decode('utf-8')
     return re.sub(r'[\=\+\/]', lambda m: {'+': '-', '/': '_', '=': ''}[m.group(0)], rv)
 
+def solver(quantity, directory, start_idx=0, prefix="", suffix="", digits=5, Random=False, Greedy=False, AStar=False, csvfilename=None):
+
+    # ensure that at least one solver was selected in the args
+    assert (Random or Greedy or AStar), "At least one solver must be selected. Solvers are Random, Greedy, and AStar"
+
+    # set up the solvers
+    solvers = [{'name': "Random Choice", 'function': JS.random_solution, 'solve': Random},
+               {'name': "Greedy", 'function': JS.greedy, 'solve': Greedy},
+               {'name': "AStar", 'function': JS.AStar, 'solve': AStar}]
+
+    # set up CSV filename if not provided
+    if csvfilename is None:
+        csvfilename = f'solutions_{prefix}{suffix}'.replace("__", "_")
+    # save solutions as CSV file
+    csvfilename = os.path.join(directory, f'{csvfilename}.csv')
+
+    # prepare the CSV file
+    csv_content = []
+    csv_row = ["filename", "total reward"]
+    for solver in solvers:
+        if solver['solve']:
+            csv_row.append(solver['name'])
+    csv_row.append("solution")
+    with open(csvfilename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_row)
+
+
+
+    for i in range(start_idx, start_idx+quantity):
+        try:
+            # load the appropriate problem from the file
+            filename = prefix + str(i).zfill(digits) + suffix + ".json"
+            simProblem = PG.loadProblem(os.path.join(directory, filename))
+
+            # determine problem stats
+            selectable_opportunities = np.sum(simProblem['Opportunities'][:, :, JF.OpportunityFeatures.SELECTABLE])
+            rewards_available = sum(simProblem['Targets'][:, JF.TaskFeatures.VALUE])
+
+            # solve problem using all selected solvers
+            csv_row = [filename, rewards_available]
+            start_time = time.time()
+            for solver in solvers:
+                if solver['solve']:
+                    solution, g = solver['function'](simProblem)
+                    csv_row.append((rewards_available-g))
+            end_time = time.time()
+
+            # record the last generated solution (should usually be from A*) in the CSV file
+            # this is done at each step to avoid losing data if the solvers hangs on a problem
+            csv_row.append(solution)
+            with open(csvfilename, 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow(csv_row)
+
+        # allow keyboard interrupts
+        except KeyboardInterrupt:
+            input("Press Enter to attempt again, or ctrl+c to quit.")
 
 def generate_dataset(effectors, targets, quantity, solve_problems=False, directory_arg=None, prefix="", suffix="", digits=5):
     """
@@ -51,7 +109,7 @@ def generate_dataset(effectors, targets, quantity, solve_problems=False, directo
         if solver['solve']:
             csv_row.append(solver['name'])
     csv_row.append("solution")
-    csv_content.append(csv_row)
+
     for i in range(quantity):
         try:
             filename = prefix + str(i).zfill(digits) + suffix + ".json"
